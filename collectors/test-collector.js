28b36aa1b6738f8398cd80b337dd5c423a970d09
Arron.Harden <arron.harden@uk.ibm.com>
2019-08-21 13:36:58 +0100

    update test collector to use rnd deltas

--- collectors/test-collector.js
+++ collectors/test-collector.js
@@ -2,18 +2,25 @@ class TestCollector {
   constructor (alias, config) {
     this.alias = alias
     this.config = config
+    this.last = {
+      temperature: 32.09,
+      humidity: 34.851083883116694,
+      pressure: 1010.918480644477
+    }
   }
 
   collect () {
     // Generate dummy data
-    return Promise.resolve({
+    const next = {
       alias: this.alias,
       type: this.getType(),
       timestamp: (new Date()).toISOString(),
-      temperature: 32.09,
-      humidity: 34.851083883116694,
-      pressure: 1010.918480644477
-    })
+      temperature: this._getNext(this.last.temperature, 1, -10, 40),
+      humidity: this._getNext(this.last.humidity, 2, 0, 100),
+      pressure: this._getNext(this.last.pressure, 50, 800, 1200)
+    }
+    this.last = next
+    return Promise.resolve(next)
   }
 
   getType () {
@@ -23,6 +30,12 @@ class TestCollector {
   init () {
     // No-op
     return Promise.resolve()
+  }
+
+  _getNext (current, maxChange, min, max) {
+    const delta = (Math.random() * maxChange) - (maxChange / 2)
+    const next = Math.min(max, Math.max(min, current + delta))
+    return next
   }
 }
 
