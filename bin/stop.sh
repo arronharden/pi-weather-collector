df5950f4101d5f19f9f8556feb32a8fd5d779d6d
Arron.Harden <arron.harden@uk.ibm.com>
2019-08-21 21:49:00 +0100

    update stop

--- bin/stop.sh
+++ bin/stop.sh
@@ -11,11 +11,11 @@ then
     EC="$?"
     if [ ${EC} -ne 0 ]
         then 
-        echo "Failed to stop process ${PID} (kill exit code of ${EC}). Please manually clean up node processes and remove pid file ${PID_FILE}".
-        exit 20
+        echo "Failed to stop process ${PID} (kill exit code of ${EC}). Please manually clean up node processes. PID file ${PID_FILE} will be removed.".
+    else
+        echo "The pi-weather-collector process was successfully stopped."
     fi
     rm -f ${PID_FILE}
-    echo "The pi-weather-collector process was successfully stopped."
 
 else
     echo "The pi-weather-collector process does not appear to be running (no PID file)."
