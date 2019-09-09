ab8502245605c3a508fe67a802450d905638317d
Arron.Harden <arron.harden@uk.ibm.com>
2019-09-09 16:21:57 +0100

    updates

--- persistence/postgres-client.js
+++ persistence/postgres-client.js
@@ -1,15 +1,18 @@
 const { Pool } = require('pg')
-const appConfig = require('../config/app-config.json')
-
-const CREATE_TABLE = `create table if not exists ${appConfig.postgres.tableName} (id serial primary key, alias text, type text, timestamp text, temperature numeric, humidity numeric, pressure numeric);`
-const INSERT_ROW = `INSERT INTO ${appConfig.postgres.tableName}(alias, type, timestamp, temperature, humidity, pressure) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`
+const appConfig = require('../config')
 
 let pool
 
 module.exports.init = function () {
-  if (!appConfig.postgres.tableName) {
-    return Promise.reject(new Error('No table name defined'))
+  if (!appConfig.postgres) {
+    return Promise.reject(new Error('No postgres config defined'))
   }
+  const reqAttrs = ['user', 'password', 'host', 'tableName', 'database']
+  reqAttrs.forEach((attr) => {
+    if (!appConfig.postgres[attr]) {
+      return Promise.reject(new Error(`Missing required attribute ${attr} in postgres config`))
+    }
+  })
   const config = {
     user: appConfig.postgres.user,
     host: appConfig.postgres.host,
@@ -21,6 +24,7 @@ module.exports.init = function () {
   pool.on('error', (err, client) => {
     console.error(`Postgres error: ${err}`, err)
   })
+  const CREATE_TABLE = `create table if not exists ${appConfig.postgres.tableName} (id serial primary key, alias text, type text, timestamp text, temperature numeric, humidity numeric, pressure numeric);`
   return pool.query(CREATE_TABLE)
     .then(() => {
       const info = Object.assign({}, config, { password: '********', tableName: appConfig.postgres.tableName })
@@ -30,5 +34,6 @@ module.exports.init = function () {
 
 module.exports.write = function (data) {
   const values = [data.alias, data.type, data.timestamp, data.temperature, data.humidity, data.pressure]
+  const INSERT_ROW = `INSERT INTO ${appConfig.postgres.tableName}(alias, type, timestamp, temperature, humidity, pressure) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`
   return pool.query(INSERT_ROW, values)
 }
